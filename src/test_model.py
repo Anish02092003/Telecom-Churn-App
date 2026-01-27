25842acbdfef286881426a7e5a9a627ac36093d7
Pritish Kumar Lenka <pritishlenka11@gmail.com>
2026-01-27 14:19:42 +0530

    Update test_model.py

--- src/test_model.py
+++ src/test_model.py
@@ -2,15 +2,12 @@ import pickle
 import pandas as pd
 import numpy as np
 
-# Load artifacts
 model = pickle.load(open("models/churn_model.pkl", "rb"))
 scaler = pickle.load(open("models/scaler.pkl", "rb"))
 feature_names = pickle.load(open("models/feature_names.pkl", "rb"))
 
-# Create empty dataframe with correct columns
 input_df = pd.DataFrame(0, index=[0], columns=feature_names, dtype=float)
 
-# Example customer (RAW values)
 customer = {
     "SeniorCitizen": 0,
     "Partner": 1,
@@ -23,16 +20,14 @@ customer = {
     "Contract_Month-to-month": 1
 }
 
-# Fill defaults with 0
 input_df.loc[0] = 0
 
-# Fill actual values
 for key, value in customer.items():
     if key in input_df.columns:
         input_df.loc[0, key] = float(value)
 
-# Scale & predict
 scaled_input = scaler.transform(input_df)
 prediction = model.predict(scaled_input)[0]
 
 print("Prediction:", "CHURN ❌" if prediction == 1 else "NO CHURN ✅")
+
